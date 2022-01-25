760be5860b814f59970a19d4d51babe72575105b
tongyaxi <32674906+tongyaxi@users.noreply.github.com>
2022-01-25 11:38:55 +0900

    Update myblog.sql

--- myblog.sql
+++ myblog.sql
@@ -17,6 +17,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXISTS my_blog;
+Use my_blog;
 -- ----------------------------
 -- Table structure for tb_article
 -- ----------------------------
